01c76dc0246e5f428524685724bb49de3cfbeda5
Swapnil Mali <swapnilmalic07@gmail.com>
2024-05-13 11:50:58 +0530

    demo project initial commit

---
+++ DemoApp/src/main/java/com/firstSpringApp/DemoApp/rest/restController.java
@@ -0,0 +1,13 @@
+package com.firstSpringApp.DemoApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello Swapnil";
+    }
+}
