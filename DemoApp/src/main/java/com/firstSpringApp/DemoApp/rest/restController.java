98abb97957b149c10ef356e079a64903c046d566
Swapnil Mali <swapnilmalic07@gmail.com>
2024-06-17 16:41:16 +0530

    Property injection

--- DemoApp/src/main/java/com/firstSpringApp/DemoApp/rest/restController.java
+++ DemoApp/src/main/java/com/firstSpringApp/DemoApp/rest/restController.java
@@ -1,13 +1,18 @@
 package com.firstSpringApp.DemoApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restController {
 
-    @GetMapping("/wecome")
+    //Inject value from properties file
+    @Value("${developer.name}")
+    String name;
+
+    @GetMapping("/welcome")
     public String sayHello() {
-        return "Hello Swapnil";
+        return "Hello "+name;
     }
 }
